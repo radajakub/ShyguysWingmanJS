b8bf4fcff74337653d39aacc69ab8e8c9c372bc3
Jakub Rada <dev.jakubrada@icloud.com>
2025-01-25 20:56:08 +0100

    Add basic interaction with ShyguyLLM

--- src/game.js
+++ src/game.js
@@ -1,19 +1,20 @@
+import { Shyguy } from "./shyguy.js";
 import { GameEngine } from "./game_engine.js";
 import { ShyGuyLLM } from "./shyguy_llm.js";
 import { StoryEngine } from "./story_engine.js";
 
 export class Game {
   constructor() {
-    this.gameEngine = new GameEngine();
-    this.shyguyLLM = new ShyGuyLLM();
-    this.storyEngine = new StoryEngine();
+    this.shyguy = new Shyguy();
+
+    this.shyguyLLM = new ShyGuyLLM(this.shyguy);
+    this.storyEngine = new StoryEngine(this.shyguy);
+    this.gameEngine = new GameEngine(this.shyguy, this.shyguyLLM, this.storyEngine);
   }
 
   async run() {
     console.log("[Game]: run");
     // await this.gameEngine.loadAssets();
     this.gameEngine.init();
-    // this.gameEngine.run();
-    // initialize and start other classes
   }
 }
