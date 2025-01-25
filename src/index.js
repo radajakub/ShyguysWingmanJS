9168886c19cc95fa31555d4e52a831fa5528a328
Jakub Rada <dev.jakubrada@icloud.com>
2025-01-25 11:58:14 +0100

    Setup file structure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import GameEngine from "./game_engine.js";
+import ShyguyLLM from "./shyguy_llm.js";
+import SisterLLM from "./sister_llm.js";
+import StoryEngine from "./story_engine.js";
+
 console.log("ShyguysWingmanJS is running!");
+
+// Initialize classes
+const gameEngine = new GameEngine();
+const shyguyLLM = new ShyguyLLM();
+const sisterLLM = new SisterLLM();
+const storyEngine = new StoryEngine();
 
 // Your JavaScript code will go here
