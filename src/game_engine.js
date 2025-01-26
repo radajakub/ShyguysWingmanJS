f21c88bab00b15aafeb93c05dc4bd1b77e0d7470
David Dobas <daviddobas@seznam.cz>
2025-01-26 06:21:37 +0100

    Game_engine await

--- src/game_engine.js
+++ src/game_engine.js
@@ -850,7 +850,7 @@ export class GameEngine {
       }
 
       // TODO: save conversation history
-      this.shyguy.learnFromWingman(message);
+      await this.shyguy.learnFromWingman(message);
       console.log("[ShyguyLLM]: Next action: ", action);
       this.resolveAction(action);
     });
